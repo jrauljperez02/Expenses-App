082dc0b44d99f02598a00a5fd699b310b2dd13e3
jrauljperez02 <raul_jimenez_71@outlook.es>
2022-06-05 14:42:34 -0500

    add dynamic style, chartbar and finished project

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -52,10 +52,13 @@ const ExpenseForm = (props) => {
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
+
+    console.log('hello')
   };
 
   return (
     <form onSubmit={submitHandler}>
+      <div></div>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Write what you bought</label>
@@ -86,7 +89,10 @@ const ExpenseForm = (props) => {
           />
         </div>
       </div>
+
+      
       <div className='new-expense__actions'>
+        <button type='button' onClick={props.onCancel} >Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
