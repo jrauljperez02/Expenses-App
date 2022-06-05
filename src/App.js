f29ec426a920473ecbc20079803958ba04a3b0f7
jrauljperez02 <raul_jimenez_71@outlook.es>
2022-06-04 22:23:39 -0500

    implement dynamic list using map of the elements

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
+import React, { useState } from 'react';
 
-import Expenses from './components/Expeses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
-import ExpensesFilter from './components/NewExpense/ExpensesFIlter'
+import Expenses from './components/Expeses/Expenses';
 
-function App() {
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-  
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-
-  const expenses = [
-    {id:'e1',title: 'Toilet Paper', amount: 90.12, date: new Date(2020,7,14)},
-    {id:'e2',title: 'New TV', amount: 799.49, date: new Date(2021,2,12)},
-    {id:'e3',title: 'Car Insurance', amount: 294.67, date: new Date(2022,2,28)},
-    {id:'e4',title: 'New Desk (Wooden)', amount: 450, date: new Date(2022,3,10)},
-    {id:'e5',title: 'Food ',amount:230,date: new Date(2022,2,5)}
-  ];
-
-  const addExpenseHandler = expense => {
-    console.log('In App.js')
-    console.log(expense)
-  }
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
-      <NewExpense onAddExpense = {addExpenseHandler}/>
-      <ExpensesFilter/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
-  
     </div>
   );
-}
+};
 
 export default App;
